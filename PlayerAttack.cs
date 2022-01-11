f1908300c527628aae9f16d8be85849608b38304
Vanessa <71130347+vanessanavacamal@users.noreply.github.com>
2022-01-10 20:04:10 -0800

    Create PlayerAttack.cs

---
+++ PlayerAttack.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private GameObject attackArea = default;
+    private bool attacking = false;
+    private float timeToAttack = 0.25f;
+    private float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attackArea = transform.GetChild(2).gameObject;   //Attack Area child (ignore first 2 children)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // mouse click!
+        {
+            Attack(); 
+        }
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeToAttack)
+            {
+                attacking = false;
+                attackArea.SetActive(attacking);
+            }
+        }
+    }
+
+    private void Attack()
+    {
+        attacking = true;
+        attackArea.SetActive(attacking);
+    }
+}
